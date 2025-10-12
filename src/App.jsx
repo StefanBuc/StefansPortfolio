34cb0f60fe5ebf20b4f2e57df356f3138cf0f706
Stefan Bucataru <andrew.bucataru@gmail.com>
2025-10-11 22:34:18 -0400

    feat: add About, Skills, Projects, and Contact sections to homepage; update App routing and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from '@/pages/Home'
-import { NotFound } from '@/pages/NotFound'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "@/pages/Home";
+import { NotFound } from "@/pages/NotFound";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>      
+        </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,25 @@ import { ThemeToggle } from "@/components/ThemeToggle"
 import { StarBackground } from "@/components/StarBackground"
 import { Navbar } from "@/components/Navbar"
 import { HeroSection } from "@/components/HeroSection"
+import { About } from "../components/About"
+import { SkillsSection } from "../components/SkillsSection"
+import { ProjectsSection } from "../components/ProjectsSection"
+import { ContactSection } from "../components/ContactSection"
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        {/* Theme Toggle */}
         <ThemeToggle />
-        {/* Background */}
+
         <StarBackground />
 
-        {/* Navbar */}
         <Navbar />
-        {/* Main Content */}
+
         <main>
             <HeroSection />
+            <About />
+            <SkillsSection />
+            <ProjectsSection />
+            <ContactSection />
         </main>
-        {/* Footer */}
     </div>
 }
